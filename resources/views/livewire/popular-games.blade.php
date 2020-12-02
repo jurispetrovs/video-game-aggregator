61b7771e7fc40de5538676bc4b51f3a47401f64b
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-02 18:31:34 +0200

    Episode 8

--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -23,6 +23,13 @@
             </div>
         </div>
     @empty
-        <div class="spinner mt-8"></div>
-    @endforelse
+        @foreach(range(1, 12) as $game)
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <div class="bg-gray-800 w-44 h-56"></div>
+                </div>
+                <div class="block text-transparent text-lg bg-gray-700 rounded leading-tight mt-4">Title goes here</div>
+                <div class="text-transparent bg-gray-700 rounded inline-block mt-3">PS4, PC, Switch</div>
+            </div>
+        @endforeach
 </div> <!-- end popular-games -->
